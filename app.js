3a54683c46feccbe59f6bc21a0cd4ee28a97a967
Witchavee <www.prim2547@gmail.com>
2025-11-08 15:16:11 +0700

    updated OCR

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ const IAM_ENDPOINT = 'https://iam.ap-southeast-2.myhuaweicloud.com/v3/auth/token
 const OCR_ENDPOINT = 'https://ocr.ap-southeast-2.myhuaweicloud.com';
 
 // ✅ From "My Credentials" page screenshot:
-const OCR_PROJECT_ID = 'd45758b291e482a95b25423703d7733'; // ⚠️ CORRECTED!
+const OCR_PROJECT_ID = 'd457f36b291e482a95b25423703d7733'; // ⚠️ CORRECTED!
 const HUAWEI_ACCOUNT_NAME = 'hid_ig0eor204azdqfu'; // Account Name (same as IAM Username)
 const HUAWEI_IAM_USERNAME = 'hid_ig0eor204azdqfu'; // IAM Username
 const HUAWEI_IAM_PASSWORD = 'credi1234'; // Your IAM password
