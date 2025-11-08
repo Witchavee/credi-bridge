0355d72fbfaeb5bf184883cb75c2d576812e9101
Witchavee <www.prim2547@gmail.com>
2025-11-08 15:35:30 +0700

    updated password

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ const OCR_ENDPOINT = 'https://ocr.ap-southeast-2.myhuaweicloud.com';
 const OCR_PROJECT_ID = 'd457f36b291e482a95b25423703d7733'; // ⚠️ CORRECTED!
 const HUAWEI_ACCOUNT_NAME = 'prim_witchavee1234'; // Account Name (same as IAM Username)
 const HUAWEI_IAM_USERNAME = 'prim_witchavee1234'; // IAM Username
-const HUAWEI_IAM_PASSWORD = 'credi1234'; // Your IAM password
+const HUAWEI_IAM_PASSWORD = 'Credi_bridge_db'; // Your IAM password
 // -----------------------------------------------------------------
 
 // 4. (NEW v11!) "ฟังก์ชัน" (Function) "ขอ 'โทเค็น'" (Request 'Token')
